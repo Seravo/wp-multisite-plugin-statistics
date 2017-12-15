1bafe61fda07880c90a85a041c792257800bb434
robneu <yo@robneu.com>
2017-12-15 11:43:51 -0500

    Disabled pushState
    This stops the admin page URL query arg from being deleted

--- js/admin.js
+++ js/admin.js
@@ -2,6 +2,9 @@
 	$(function() {
 		// Make tables pretty and sortable
 		$('.plugin-usage-table').dynatable({
+			features: {
+				pushState: false
+			},
 			readers: {
 		      'integer': function(el, record) {
 		        return Number(el.innerHTML) || 0;
