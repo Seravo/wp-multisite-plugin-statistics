be8ea408cd3d852d5fe2b6867385a4768dfa5550
robneu <yo@robneu.com>
2017-12-15 11:27:27 -0500

    Removed unused ajax calls
    These don’t do anything, no point in keeping them

--- wp-multisite-plugin-statistics.php
+++ wp-multisite-plugin-statistics.php
@@ -41,10 +41,6 @@ class MultisitePluginStats {
 		add_action( 'network_admin_menu', array( $this, 'add_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
 		add_action( 'plugins_loaded', array( $this, 'localization' ) );
-		// Ajax handler
-		add_action( 'wp_ajax_deactivate_network_plugins', array( $this, 'deactivate_network_plugins' ) );
-		add_action( 'wp_ajax_deactivate_plugins', array( $this, 'deactivate_plugins' ) );
-		add_action( 'wp_ajax_delete_plugins', array( $this, 'delete_plugins' ) );
 	}
 
 	public function localization() {
@@ -60,46 +56,6 @@ class MultisitePluginStats {
 			'multisite_plugin_stats',
 			array( $this, 'stats_page' )
 		);
-	}
-
-	// Ajax Handlers
-	public function deactivate_network_plugins() {
-		$params = array();
-		parse_str( $_POST['plugins'], $params );
-
-		foreach ( $params['checked'] as $plugin ) {
-			echo 'Plugari: ' . $plugin . "\n";
-			// deactivate_plugins(plugin_basename($plugin));
-		}
-		die();
-	}
-
-	public function deactivate_plugins() {
-		global $wpdb;
-		$params = array();
-		parse_str( $_POST['plugins'], $params );
-		$plugins = $params['checked'];
-
-		set_time_limit( 120 );
-
-		$blogs = $wpdb->get_col( "SELECT blog_id FROM {$wpdb->blogs} WHERE site_id = {$wpdb->siteid} AND spam = 0" );
-		if ( $blogs ) {
-			foreach ( $blogs as $blog_id ) {
-				switch_to_blog( $blog_id );
-				foreach ( $plugins as $plugin ) {
-					deactivate_plugins( $plugin, true ); // silently deactivate the plugin
-				}
-				restore_current_blog();
-			}
-		}
-		die();
-	}
-	// TODO EI TOIMI
-	public function delete_plugins() {
-		$params = array();
-		parse_str( $_POST['plugins'], $params );
-		var_dump( $params );
-		die();
 	}
 
 	public function stats_page() {
@@ -148,40 +104,34 @@ class MultisitePluginStats {
 		}
 
 		?>
-
 		<div class='wrap'>
-		<div class="icon32" id="icon-plugins"><br></div>
-		<h2><?php _e( 'Plugin Statistics', 'multisite_plugin_stats' ); ?></h2>
-		<h3><?php _e( 'Network Activated Plugins', 'multisite_plugin_stats' ); ?> (<?php echo count( $network_plugins ); ?>)</h3>
-		<form name="ajaxform" id="deactivate-network" action="<?php admin_url( 'admin-ajax.php' ) ?>">
-		<table class="wp-list-table widefat plugin-usage-table">
-			<thead>
-				<tr>
-					<th data-dynatable-no-sort="true" class="manage-column column-cb check-column">
-						<input class="column-select-all" id="network-plugins-select-all" type="checkbox">
-					</th>
-					<th><?php _e( 'Name', 'multisite_plugin_stats' ); ?></th>
-					<th><?php _e( 'Path', 'multisite_plugin_stats' ); ?></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				foreach ( $network_plugins as $plugin => $etc ) { ?>
+			<h2><?php _e( 'Plugin Statistics', 'multisite_plugin_stats' ); ?></h2>
+			<h3><?php _e( 'Network Activated Plugins', 'multisite_plugin_stats' ); ?> (<?php echo count( $network_plugins ); ?>)</h3>
+			<table class="wp-list-table widefat plugin-usage-table">
+				<thead>
+					<tr>
+						<th data-dynatable-no-sort="true" class="manage-column column-cb check-column">
+							<input class="column-select-all" id="network-plugins-select-all" type="checkbox">
+						</th>
+						<th><?php _e( 'Name', 'multisite_plugin_stats' ); ?></th>
+						<th><?php _e( 'Path', 'multisite_plugin_stats' ); ?></th>
+					</tr>
+				</thead>
+				<tbody>
 					<?php
-					echo '<tr><th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="' . plugin_basename( $plugin ) . '" id=""></th>';
-					echo '<td>' . $plugin_info[ $plugin ]['Name'] . '</td>';
-					echo '<td>' . plugin_basename( $plugin ) . '</td></tr>';
-					// Remove it from the list
-					unset( $plugin_info[ $plugin ] );
-				}
-				?>
-			</tbody>
-		</table>
-		<?php submit_button( __( 'Deactivate' ),'secondary', 'deactivate-network-plugins' ); ?>
-		</form>
+					foreach ( $network_plugins as $plugin => $etc ) { ?>
+						<?php
+						echo '<tr><th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="' . plugin_basename( $plugin ) . '" id=""></th>';
+						echo '<td>' . $plugin_info[ $plugin ]['Name'] . '</td>';
+						echo '<td>' . plugin_basename( $plugin ) . '</td></tr>';
+						// Remove it from the list
+						unset( $plugin_info[ $plugin ] );
+					}
+					?>
+				</tbody>
+			</table>
 
-		<h3><?php _e( 'Active Plugins', 'multisite_plugin_stats' ); ?> (<?php echo count( $active_plugins ); ?>)</h3>
-		<form name="ajaxform" id="deactivate-plugins" action="<?php admin_url( 'admin-ajax.php' ) ?>">
+			<h3><?php _e( 'Active Plugins', 'multisite_plugin_stats' ); ?> (<?php echo count( $active_plugins ); ?>)</h3>
 			<table class="wp-list-table widefat plugin-usage-table">
 				<thead>
 					<tr>
@@ -195,42 +145,39 @@ class MultisitePluginStats {
 					</tr>
 				</thead>
 				<tbody>
-			<?php
-				$counter = 0;
-			foreach ( $active_plugins as $plugin => $blog_array ) {
-				if ( empty( $plugin_info[ $plugin ] ) ) {
-					continue;
-				}
-				echo '<tr><th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="' . plugin_basename( $plugin ) . '" id=""></th>';
+					<?php
+					$counter = 0;
+					foreach ( $active_plugins as $plugin => $blog_array ) {
+						if ( empty( $plugin_info[ $plugin ] ) ) {
+							continue;
+						}
+						echo '<tr><th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="' . plugin_basename( $plugin ) . '" id=""></th>';
 
-				echo '<td>' . $plugin_info[ $plugin ]['Name'] . '</td>';
-				echo '<td>' . plugin_basename( $plugin ) . '</td>';
-				echo '<td>' . count( $blog_array ) . '</td>';
+						echo '<td>' . $plugin_info[ $plugin ]['Name'] . '</td>';
+						echo '<td>' . plugin_basename( $plugin ) . '</td>';
+						echo '<td>' . count( $blog_array ) . '</td>';
 
-				// List the sites
-				echo '<td> ';
-				$output = '';
-				foreach ( $blog_array as $blog_id ) {
-					if ( $output === '' ) { // first round
-						$output .= '<a href="' . get_site_url( $blog_id ) . '/wp-admin/plugins.php">' . htmlspecialchars( $site_names[ $blog_id ] ) . '</a>';
-					} else { $output .= ' , ' . '<a href="' . get_site_url( $blog_id ) . '/wp-admin/plugins.php">' . htmlspecialchars( $site_names[ $blog_id ] ) . '</a>';
+						// List the sites
+						echo '<td> ';
+						$output = '';
+						foreach ( $blog_array as $blog_id ) {
+							if ( $output === '' ) { // first round
+								$output .= '<a href="' . get_site_url( $blog_id ) . '/wp-admin/plugins.php">' . htmlspecialchars( $site_names[ $blog_id ] ) . '</a>';
+							} else { $output .= ' , ' . '<a href="' . get_site_url( $blog_id ) . '/wp-admin/plugins.php">' . htmlspecialchars( $site_names[ $blog_id ] ) . '</a>';
+							}
+						}
+						echo "{$output} </td>";
+						echo '</tr>';
+
+						// Remove it from the list
+						unset( $plugin_info[ $plugin ] );
+						$counter++;
 					}
-				}
-				echo "{$output} </td>";
-				echo '</tr>';
-
-				// Remove it from the list
-				unset( $plugin_info[ $plugin ] );
-				$counter++;
-			}
-			?>
-			</tbody>
+					?>
+				</tbody>
 			</table>
-			<?php submit_button( __( 'Deactivate' ),'secondary', 'deactivate-plugins' ); ?>
-		</form>
 
-		<h3><?php _e( 'Inactive Plugins', 'multisite_plugin_stats' ); ?> (<?php echo count( $plugin_info ); ?>)</h3>
-		<form name="ajaxform" id="delete-plugins" action="<?php admin_url( 'admin-ajax.php' ) ?>">
+			<h3><?php _e( 'Inactive Plugins', 'multisite_plugin_stats' ); ?> (<?php echo count( $plugin_info ); ?>)</h3>
 			<table class="wp-list-table widefat plugin-usage-table">
 				<thead>
 					<tr>
@@ -242,18 +189,15 @@ class MultisitePluginStats {
 					</tr>
 				</thead>
 				<tbody>
-			<?php
-			foreach ( $plugin_info as $plugin => $info ) {
-				echo '<tr><th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="' . plugin_basename( $plugin ) . '" id=""></th>';
-				echo '<td>' . $info['Name'] . '</td>';
-				echo '<td>' . plugin_basename( $plugin ) . '</td></tr>';
-			}
-			?>
+					<?php
+					foreach ( $plugin_info as $plugin => $info ) {
+						echo '<tr><th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="' . plugin_basename( $plugin ) . '" id=""></th>';
+						echo '<td>' . $info['Name'] . '</td>';
+						echo '<td>' . plugin_basename( $plugin ) . '</td></tr>';
+					}
+					?>
 				</tbody>
 			</table>
-			<?php submit_button( __( 'Delete Permanently' ),'delete plugins', 'delete-plugins' ); ?>
-		</form>
-
 		</div> <!-- .wrap -->
 	<?php
 	}
@@ -261,21 +205,13 @@ class MultisitePluginStats {
 	/**
 	 * Registers and enqueues admin-specific JavaScript.
 	 */
-	public function register_admin_scripts() {
+	public function register_admin_scripts( $page ) {
 		wp_enqueue_script(
 			'multisite-plugin-stats-admin-script',
 			"{$this->plugin_url}js/admin.js",
 			array( 'jquery' ),
 			self::VERSION,
 			true
-		);
-		wp_localize_script(
-			'multisite-plugin-stats-admin-script',
-			'ajax_object',
-			array(
-				'ajax_url' => admin_url( 'admin-ajax.php' ),
-				'nonce' => wp_create_nonce( 'bulk-plugins' ),
-			)
 		);
 
 		wp_enqueue_script(
